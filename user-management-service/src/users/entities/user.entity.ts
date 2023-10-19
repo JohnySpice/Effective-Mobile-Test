01cf7a21ea7bc647776118782c75a8d0b8bfa1a4
Viacheslav Tiulkov <vya9977@yandex.ru>
2023-10-19 23:29:20 +0300

    feat: update user entity with typeorm decorators

--- user-management-service/src/users/entities/user.entity.ts
+++ user-management-service/src/users/entities/user.entity.ts
@@ -1,1 +1,48 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ColumnOptions,
+} from 'typeorm';
+
+const dateColumnsOptions = {
+  type: 'timestamp',
+  transformer: {
+    from: (value: Date) => Date.parse(value.toISOString()),
+    to: (value) => value,
+  },
+} as ColumnOptions;
+
+enum EGender {
+  MALE = 'male',
+  FEMALE = 'female',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 64, name: 'first_name' })
+  firstName: string;
+
+  @Column({ length: 64, nullable: true, name: 'last_name' })
+  lastName: string;
+
+  @Column({ enum: EGender })
+  gender: string;
+
+  @Column({ length: 128 })
+  email: string;
+
+  @Column({ type: 'smallint' })
+  age: number;
+
+  @CreateDateColumn(dateColumnsOptions)
+  createdAt: number;
+
+  @UpdateDateColumn(dateColumnsOptions)
+  updatedAt: number;
+}
