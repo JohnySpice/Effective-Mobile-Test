4a7f1c9f4ef1be5997d664605ef41de38b76f16b
Viacheslav Tiulkov <vya9977@yandex.ru>
2023-10-22 21:58:57 +0300

    feat: add config for starting redis

--- user-management-service/src/redis/redis.service.ts
+++ user-management-service/src/redis/redis.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import Redis from 'ioredis';
 import { IHistoryRecord } from 'src/interfaces/history-record.interface';
+import 'dotenv/config';
 
 @Injectable()
 export class RedisService extends Redis {
   private channel = 'user-changes';
-
+  constructor() {
+    super(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST);
+  }
   async pub(data: IHistoryRecord) {
     await super.publish(this.channel, JSON.stringify(data));
   }
